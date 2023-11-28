44782dab8f9ea505bf70e9bcc774944f11909ea0
nubik132 <mister.nubik@list.ru>
2023-11-28 19:05:00 +0300

    feat: add getDataFromFile method implementation

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,28 @@
 package com.epam.mjc.io;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
+        try (java.io.FileReader in = new java.io.FileReader(file)){
+            String profileLine;
+            String[] buffer = new String[4];
+
+            try (BufferedReader br = new BufferedReader(in)) {
+                for(int i = 0; (profileLine = br.readLine()) != null; i++){
+                    buffer[i] = profileLine.split("\\s")[1];
+                }
+            }
+
+            return new Profile(buffer[0], Integer.parseInt(buffer[1]),
+                    buffer[2], Long.parseLong(buffer[3]));
+        } catch (IOException e){
+            e.printStackTrace();
+        }
         return new Profile();
     }
 }
